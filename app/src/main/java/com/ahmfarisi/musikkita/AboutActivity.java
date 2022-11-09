612fcc94422144926a605f3f1877c90ad9c27619
ahmfarisilect <ahmfarisi@gmail.com>
2022-11-09 11:25:55 +0700

    Finish Project

---
+++ app/src/main/java/com/ahmfarisi/musikkita/AboutActivity.java
@@ -0,0 +1,14 @@
+package com.ahmfarisi.musikkita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
